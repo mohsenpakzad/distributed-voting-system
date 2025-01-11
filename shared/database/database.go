4d164f8310a1f99fc417f599c80f1bc0bc89b2a0
Mohsen Pakzad <mohsen.pakzad99@gmail.com>
2025-01-11 13:07:19 +0330

    feat: add new method for closing db

--- shared/database/database.go
+++ shared/database/database.go
@@ -15,10 +15,20 @@ func ConnectDB(dbUrl string) *gorm.DB {
 		log.Fatalf("failed to connect database: %v", err)
 	}
 
-	err = db.AutoMigrate(&models.User{}, &models.Election{}, &models.Candidate{}, &models.Vote{})
+	err = db.AutoMigrate(
+		&models.User{}, &models.Election{}, &models.Candidate{}, &models.Vote{},
+	)
 	if err != nil {
 		log.Fatal("failed to automigrate:", err)
 	}
 
 	return db
+}
+
+func CloseDB(db *gorm.DB) {
+	d, err := db.DB()
+	if err != nil {
+		log.Fatalf("failed to get database connection: %v", err)
+	}
+	d.Close()
 }
